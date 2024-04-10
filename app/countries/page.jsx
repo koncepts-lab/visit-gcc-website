af9f0e0a5d9c2bff582c3ad3e1da540cda7f9b09
AdilKL <adil.m@konceptslab.com>
2024-04-09 17:11:53 -1200

    new page

---
+++ app/countries/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CountryBanner from '../../components/countries/banner/CountryBanner'
+import Requirments from '@/components/countries/requirments/Requirments'
+
+
+const page = () => {
+  return (
+    <div>
+      <CountryBanner/>
+      <Requirments/>
+
+    </div>
+  )
+}
+
+export default page
---
+++ components/countries/requirments/Requirments.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './requirments.css'
+
+function Requirments() {
+  return (
+    <div className='requirments'>
+      
+        <div className='container'>
+            <h3>Visa Requirments</h3>
+        </div>
+
+    </div>
+  )
+}
+
+export default Requirments
