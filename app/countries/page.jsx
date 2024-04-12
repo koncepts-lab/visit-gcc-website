4023952e84a171365855a309cc80ea59e55dc5fb
Adil M <adilmullappally786@gmail.com>
2024-04-12 20:05:45 +0530

    new sec

--- app/countries/page.jsx
+++ app/countries/page.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import CountryBanner from '../../components/countries/banner/CountryBanner'
 import Requirments from '@/components/countries/requirments/Requirments'
 import About from '@/components/countries/about/About'
+import Tabs from '@/components/countries/popular spot/tab'
+
+
 
 
 const page = () => {
@@ -10,6 +13,9 @@ const page = () => {
       <CountryBanner/>
       <Requirments/>
       <About/>
+      <Tabs/>
+      
+      
 
     </div>
   )
