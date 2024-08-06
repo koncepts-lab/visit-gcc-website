63f12560aa1b9b1d21ea44f149f30bc2154b4405
nithin <developer@konceptslab.com>
2024-08-06 18:20:21 +0530

    country page responsive

--- components/countries/single-countries.js
+++ components/countries/single-countries.js
@@ -6,7 +6,7 @@ const ArrowIcon = () => <FaArrowRightLong />;
 
 const SingleCountry = (props) => {
   return (
-    <div className="col-xxl-2 col-xl-2 col-lg-4 col-md-2 col-sm-2 col-6">
+    <div className="col-xxl-2 col-xl-2 col-lg-4 col-md-4 col-sm-2 col-6">
       <img src={props.image} alt={props.heading} />
       <div className={style['title-countries']}>
         <h4>{props.heading}</h4>
